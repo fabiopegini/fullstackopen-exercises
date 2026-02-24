24abb893ad1701c010fec4b069ca1f608bc54eab
fabiopegini <fipegini@gmail.com>
2026-02-24 23:15:54 +0100

    Solution for exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,24 +4,39 @@ const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</bu
 
 const Display = ({text, number}) => <p>{text + " " + number}</p>
 
+
 const App = () => {
   const [goodReviews, setGoodReviews] = useState(0)
   const [neutralReviews, setNeutralReviews] = useState(0)
   const [badReviews, setBadReviews] = useState(0)
+  const [allReviews, setAllReviews] = useState(0)
+  const [averageReviews, setAverageReviews] = useState(0)
+  const [goodPercentage, setGoodPercentage] = useState(0)
+  
+  const handleClick = (newReviewValue, addReview, addToAverage) => {
+    addReview(newReviewValue)
+    setAllReviews(allReviews + 1)
+    setAverageReviews((goodReviews - badReviews + addToAverage) / (allReviews + 1))
+    if(addToAverage > 0) setGoodPercentage(newReviewValue * 100 / (allReviews + 1))
+    if(addToAverage <= 0) setGoodPercentage(goodReviews * 100 / (allReviews + 1))
+  }
 
   return (
     <>
       <div>
         <h1>Give feedback</h1>
-        <Button text={"bad"} handleClick={() => setBadReviews(badReviews + 1)} />
-        <Button text={"neutral"} handleClick={() => setNeutralReviews(neutralReviews + 1)} />
-        <Button text={"good"} handleClick={() => setGoodReviews(goodReviews + 1)} />
+        <Button text={"bad"} handleClick={() => handleClick(badReviews + 1, setBadReviews, -1)} />
+        <Button text={"neutral"} handleClick={() => handleClick(neutralReviews + 1, setNeutralReviews, 0)} />
+        <Button text={"good"} handleClick={() => handleClick(goodReviews + 1, setGoodReviews, 1)} />
       </div>
       <div>
         <h1>Statistics</h1>
         <Display text={"good"} number={goodReviews} />
         <Display text={"neutral"} number={neutralReviews} />
         <Display text={"bad"} number={badReviews} />
+        <Display text={"all"} number={allReviews} />
+        <Display text={"average"} number={averageReviews} />
+        <Display text={"positive"} number={goodPercentage} />
       </div>
   </>
   )
